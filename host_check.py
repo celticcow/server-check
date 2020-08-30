d7daccc29806a7c21475c3881bee41df23031f56
Celticcow <gregory.dunlap@gmail.com>
2020-08-30 10:44:43 -0500

    arg parse and connect

--- host_check.py
+++ host_check.py
@@ -31,9 +31,23 @@ def extract_data(file):
                         # port is not there ... so skip it.
                         pass
                     else:
-                        print("Connect to ", end="")
-                        print(dst, end="")
-                        print(" port: " + row[x], end=end)
+                        port = str(validate_port(row[x]))
+
+                        if(port != 0):
+                            #print("Connect to ", end="")
+                            #print(dst, end="")
+                            #print(" port: " + row[x], end=" ")
+                            #print(" " + port, end=end)
+
+                            if(connection(dst, port)):
+                                print("** Valid ", end=" ")
+                            else:
+                                print("** Not_Found: ", end=" ")
+                            
+                            print("Connect to ", end="")
+                            print(dst, end="")
+                            print(" port: " + row[x], end=" ")
+                            print(" " + port, end=end)
                         #print(row[x] + " port")
             #print(dst, end=end)
             #print(total, end=end)
@@ -54,17 +68,58 @@ def connection(server, port):
         return(False)
 #end of connect
 
+def validate_port(port_str):
+    debug = 1
 
+    if(port_str == "http"):
+        return("80")
+    elif(port_str == "https"):
+        return("443")
+    elif(port_str == "ftp"):
+        return("21")
+    elif(port_str == "ssh"):
+        return("22")
+    elif(port_str == "traceroute"):
+        return("0")
+    elif("-" in port_str):
+        portsplit = port_str.split("-")
+
+        if(portsplit[0].lower() == "udp"):
+            return(0)
+        else:
+            return(portsplit[1])
+    elif(port_str.isnumeric()):
+        # is number
+        return(int(port_str))
+    else:
+        ##default case ... nothing found return 0
+        return(0)
 
 def main():
     print("In Main Function")
 
-    extract_data("sample.csv")
+    parser = argparse.ArgumentParser(description='Port Check')
 
+    parser.add_argument("-f", required=True, help="name of input file(csv)")
+
+    args = parser.parse_args()
+
+    inputfile = args.f
+
+    extract_data(inputfile)
+
+    """
     if(connection("146.18.2.137", "22")):
         print("good")
     else:
         print("failed")
+    
+    print(validate_port("http"))
+    print(validate_port("https"))
+    print(validate_port("http-7040"))
+    print(validate_port("TCP-9999"))
+    print(validate_port("UdP-9999"))
+    """
 
 if __name__ == "__main__":
     main()
